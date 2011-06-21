5b39ef3dd7a39ca461fa0bce0d1413e049c776ae
Robert Krombholz <robertkrombholz@gmx.de>
2011-06-21 23:51:04 +0200

    * Correcting bootstrap data to use the saveTask & moveTask service methog instead of creating the raw domain objects

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,7 @@
 import app.taskboard.*
 import grails.util.Environment
 import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+import groovy.time.TimeCategory
 
 class BootStrap {
 	def springSecurityService
@@ -41,44 +42,61 @@ class BootStrap {
 			log.info 'Creating test data...'			
 			
 			def user = User.findByUsername('user')
-			def admin = User.findByUsername('admin')			
-			def testTasks = [
-				wip1: 	[name:'Bootstrap Webserver', description:'Bootstrap machine and apply WebServer profile.',durationHours:3.5, creator:user, assignee:user, sortorder:1, priority:'Critical', color:'#fa7a88'],
-				todo1: 	[name:'Setup Webserver', description:'Install & configure Apache',durationHours:1.5, creator:user, assignee:user, sortorder:1, priority:'Major', color:'#faf77a'],
-				wip2: 	[name:'Bootstrap App server', description:'Bootstrap machine and apply Java Appserver profile', durationHours:4.0, creator:user, assignee:user, sortorder:2, priority:'Normal', color:'#faf77a'],
-				todo2: 	[name:'Setup WebLogic', description:'Install base software and configure domain.',durationHours:6.0, creator:user, assignee:user, sortorder:2, priority:'Low', color:'#f9d7a9'],
-				todo3: 	[name:'Install application', description:'Install the application software as describes by the Vendor',durationHours:8.0, creator:user, assignee:user, sortorder:3, priority:'Critical', color:'#faf77a'],
-				todo4: 	[name:'Configure Monitoring', description:'Setup all monitors (Filesystem, proceses, logs etc)',durationHours:16.0, creator:user, assignee:user, sortorder:4, priority:'Normal', color:'#bcbcf5'],
-				todo5: 	[name:'Apply Configuration Management', description:'Apply configuration management',durationHours:8.0, creator:user, assignee:user, sortorder:5, priority:'Normal', color:'#faf77a'],
-				done1: 	[name:'Request machines', description:'Request machines at the DataCenter',durationHours:48.0, creator:user, assignee:user, sortorder:1, priority:'Normal', color:'#bcbcf5'],
-			]
+			def admin = User.findByUsername('admin')
 			def column1 = new Column(name:'ToDo', limit:15, workflowStartColumn:true)
 			def column2 = new Column(name:'In Progress', limit:4)
 			def column3 = new Column(name:'Done!')
-			//Relate everything togather
-			column1
-				.addToTasks(testTasks.todo1)
-				.addToTasks(testTasks.todo2)
-				.addToTasks(testTasks.todo3)
-				.addToTasks(testTasks.todo4)
-				.addToTasks(testTasks.todo5)
-				
-			column2
-				.addToTasks(testTasks.wip1)
-				.addToTasks(testTasks.wip2)
-			column3
-				.addToTasks(testTasks.done1)
 			def board = new Board(
-				name:'My Test Board', 
-				description:'This test board is to track the tasks of our Test project', 				
+				name:'My Test Board',
+				description:'This test board is to track the tasks of our Test project',
 				users: [user,admin],
-				admins: [admin],				
+				admins: [admin],
 			)
 			board.addToColumns(column1)
 				.addToColumns(column2)
 				.addToColumns(column3)
 			board.save(flush:true)
 			
+			def testTasks
+
+			use(TimeCategory) {	
+				testTasks = [
+					wip: 	[
+						[column:column2,  name:'Bootstrap Webserver', description:'Bootstrap machine and apply WebServer profile.',durationHours:3.5, creator:user, assignee:user, sortorder:1, priority:'Critical', color:'#fa7a88', workflowStartDate: new Date() - 10.hours],
+						[column:column2, name:'Bootstrap App server', description:'Bootstrap machine and apply Java Appserver profile', durationHours:4.0, creator:user, assignee:user, sortorder:2, priority:'Normal', color:'#faf77a', workflowStartDate: new Date() - 9.hours]
+					],
+					todo: 	[
+						[column:column1, name:'Setup Webserver', description:'Install & configure Apache',durationHours:1.5, creator:user, assignee:user, sortorder:1, priority:'Major', color:'#faf77a', workflowStartDate: new Date() - 9.hours],					 
+						[column:column1, name:'Setup WebLogic', description:'Install base software and configure domain.',durationHours:6.0, creator:user, assignee:user, sortorder:2, priority:'Low', color:'#f9d7a9', workflowStartDate: new Date() - 7.hours],
+						[column:column1, name:'Install application', description:'Install the application software as describes by the Vendor',durationHours:8.0, creator:user, assignee:user, sortorder:3, priority:'Critical', color:'#faf77a', workflowStartDate: new Date() - 4.hours],
+						[column:column1, name:'Configure Monitoring', description:'Setup all monitors (Filesystem, proceses, logs etc)',durationHours:16.0, creator:user, assignee:user, sortorder:4, priority:'Normal', color:'#bcbcf5', workflowStartDate: new Date() - 2.hours],
+						[column:column1, name:'Apply Configuration Management', description:'Apply configuration management',durationHours:8.0, creator:user, assignee:user, sortorder:5, priority:'Normal', color:'#faf77a', workflowStartDate: new Date() - 12.hours]
+					],
+					done: 	[
+						[column:column3, name:'Request machines', description:'Request machines at the DataCenter',durationHours:48.0, creator:user, assignee:user, sortorder:1, priority:'Normal', color:'#bcbcf5', workflowStartDate: new Date() - 10.hours, workflowEndDate: new Date() - 1.hour]
+					]
+				]
+			}
+			SpringSecurityUtils.doWithAuth('user') {
+				testTasks.each {k, v -> v.each {taskService.saveTask(new Task(it))}}
+			}
+			def tmpIdList = []
+			SpringSecurityUtils.doWithAuth('user') {	
+				testTasks.wip.each {
+					def persistedTask = Task.findByName(it.name)
+					taskService.moveTask tmpIdList << persistedTask.id , persistedTask.column.id, Column.findByName('In Progress').id, persistedTask.id
+				}
+				testTasks.done.each {
+					def persistedTask = Task.findByName(it.name)
+					taskService.moveTask tmpIdList << persistedTask.id, persistedTask.column.id, Column.findByName('In Progress').id, persistedTask.id
+				}
+				testTasks.done.each {
+					def persistedTask = Task.findByName(it.name)
+					taskService.moveTask tmpIdList << persistedTask.id, persistedTask.column.id, Column.findByName('Done!').id, persistedTask.id
+				}				
+			}	
+			
+	
 			//Now we create some tasks and move them across the board in order 
 			//to get some proper event data that we can test with
 			def eventTestTasks = []
