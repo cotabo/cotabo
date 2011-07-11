6611d4deb4b8edd2cdb098d24ea1409110bc5806
Robert Krombholz <robertkrombholz@gmx.de>
2011-07-11 19:04:17 +0200

    * Removed println statement

--- grails-app/services/app/taskboard/BoardUpdateService.groovy
+++ grails-app/services/app/taskboard/BoardUpdateService.groovy
@@ -97,8 +97,7 @@ class BoardUpdateService {
 	 * @param broadcaster The Atmosphere Broadcaster object that the message should be distributed too.
 	 * @param TaskMovementMessage The message contianing information about the movement.
 	 */
-    void sendTaskMovementMessage(TaskMovementMessage message, Broadcaster broadcaster) {
-		println message as JSON		
+    void sendTaskMovementMessage(TaskMovementMessage message, Broadcaster broadcaster) {		
 		broadcaster.broadcast(message as JSON)
     }
 }
