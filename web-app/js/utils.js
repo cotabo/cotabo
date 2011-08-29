cotabo/cotabo

commit 2b7f78f184eb8ed63af34ac96728c912af134e37

* Changing icons to jQuery's

web-app/js/utils.js
@@ -116,11 +116,13 @@ var taskTpl = function () {
  * @returns
  */
 var taskBlockCallback = function(data) {
-	var taskBlockDiv = $('li#task_'+data.task).find('div.block-box');	
+	var taskBlockDiv = $('li#task_'+data.task).find('span.block-box');	
 	if((data.blocked == true) && $(taskBlockDiv).hasClass('not-blocked')) {
 		$(taskBlockDiv).removeClass('not-blocked').addClass('blocked');
+		$(taskBlockDiv).removeClass('ui-icon-unlocked').addClass('ui-icon-locked');
 	}
 	else if ((data.blocked == false) && $(taskBlockDiv).hasClass('blocked')){
-		$(taskBlockDiv).removeClass('blocked').addClass('not-blocked');		
+		$(taskBlockDiv).removeClass('blocked').addClass('not-blocked');	
+		$(taskBlockDiv).removeClass('ui-icon-locked').addClass('ui-icon-unlocked');
 	}
 }

grails-app/controllers/org/cotabo/TaskController.groovy
@@ -103,7 +103,8 @@ class TaskController {
 			boolean settedBlock = false 			
 			boolean wasBlocked = params.wasBlocked?.toBoolean()?.booleanValue()
 			//If we have a 'wasBlocked' param which equals to the current task status
-            if(taskInstance.blocked == wasBlocked) {			
+			boolean currentBlocked = taskInstance.blocked
+            if(currentBlocked == wasBlocked) {			
 				//Flip the blocked status	
 				taskInstance.blocked = !wasBlocked	
 				//saving that this was a blocked status update

grails-app/taglib/org/cotabo/BoardTagLib.groovy
@@ -70,17 +70,17 @@ class BoardTagLib {
 			out << """
 			<li class="ui-widget ui-corner-all" id="task_${attrs.task.id}">
 				<div class="task-header ui-state-default">
-					<img class="avatar" src="${createLink(controller:'user', action:'avatar', id:attrs.task?.assignee?.username)}" />
+					<img class="ui-icon ui-icon-person avatar" src="${createLink(controller:'user', action:'avatar', id:attrs.task?.assignee?.username)}" />
 					<div class="head_color" style="background-color:${attrs.task?.color?.encodeAsHTML()};"></div>
 					<div id="color_helper" style="display:none;">${attrs.task?.color?.encodeAsHTML()}</div>
 					<div class="head_name">#${attrs.task?.id ?: ''} - ${attrs.task?.name?.encodeAsHTML()}</div>"""
 				if(attrs.task.column.id == lastColumn.id) {
 					out << """
-					<a href="${createLink(controller:'task', action:'archive', id:attrs.task?.id)}" class="archive"></a>"""
+					<a href="${createLink(controller:'task', action:'archive', id:attrs.task?.id)}" class="ui-icon ui-icon-disk archive"></a>"""
 				}
 				out << """
-					<div class="block-box ${attrs.task?.blocked ? 'blocked' : 'not-blocked'}"></div>
-					<span class="ui-icon ui-icon-carat-1-${attrs.hide ? 's' : 'n'}"/>
+					<span class="block-box ui-icon ${attrs.task?.blocked ? 'ui-icon-locked blocked' : 'ui-icon-unlocked not-blocked'}" ></span>
+					<span class="expander ui-icon ui-icon-carat-1-${attrs.hide ? 's' : 'n'}" ></span>
 				</div>
 				<div class="task-content ui-widget-content" style="display:${attrs.hide ? 'none' : 'block'}">
 					<table>

test/integration/org/cotabo/TaskIntegrationTests.groovy
@@ -48,6 +48,12 @@ class TaskIntegrationTests extends GrailsUnitTestCase {
 		assertEquals 'In Progress', tmpTask.column.name
 	}
 	
+	void testInitialBlocked() {
+		def task = Task.findByName('Bootstrap Webserver')
+		
+		assertTrue task.blocked
+	}
+	
 	void testSetBlocked() {
 		//Getting a test task created in super class
 		def task = Task.findByName('Task 3')

test/unit/org/cotabo/TaskTests.groovy
@@ -12,7 +12,7 @@ class TaskTests extends TaskBoardUnitTest {
     }
 
     void testCreation() {
-		//Preperation
+		//Preparation
 		def col = Column.findByName('todo')
 		def user = User.findByUsername('testuser')				
 		assertNotNull col
@@ -260,7 +260,5 @@ class TaskTests extends TaskBoardUnitTest {
 		assertFalse task.validate()
 		assertNull task.save()
 		assertNull Task.findByName('mytask')
-	}	
-	
-	
+	}
 }

web-app/css/board.css
@@ -124,49 +124,40 @@ div.task-content * td {
 	padding: 10px 6px;
 }
 .block-box {
-	float:right;
-    top:+5px;
-    width:17px;
-    height:17px;
-    cursor:pointer; 
-    margin: 3px;    
+	display:block;
+	
+	cursor:pointer;
+	width:16px;
+    height:16px;
+    margin:2px;
+    /*top:+5px;
+    float:right;
+     
+    */
 }
 .blocked {			
-	background: url('../images/stop.png') no-repeat;
-	opacity: .7; 	
+		
 }
 .not-blocked {
-	background: url('../images/stop_greyed.png') no-repeat;
-	opacity: .3;    
+	    
 }
 
 .avatar {
-	background: url('../images/default_avatar.jpeg') no-repeat;
-	opacity: .7;
-	width: 16px; 
-	height: 16px;
-	margin: 2px;
 	display: block;
 	float: left;
-	overflow: hidden;
-	-moz-background-size: 100% 100%;
-	-o-background-size: 100% 100%;
-	-webkit-background-size: 100% 100%; 
-	background-size: 100% 100%;
+	margin: 2px;
+	width: 16px; 
+	height: 16px;
 }
 
 .archive {
-	background: url('../images/archive.jpeg') no-repeat;
-	opacity:.7;
 	float:right;
 	width:16px;
 	height:16px;
 	display:block;
-	overflow:hidden;
 	margin: 3px;
-	-moz-background-size: 100% 100%;
-	-o-background-size: 100% 100%;
-	-webkit-background-size: 100% 100%; 
-	background-size: 100% 100%;
 }
+
+.expander{
+	
 

commit 2d093e087da506cf005cf9a42f90c2c5de0c2e37

* fixing taskTpl that is used to create tasks dynamically. Got broken in 2b7f78f184eb8ed63af3

web-app/js/utils.js
@@ -58,12 +58,12 @@ var taskTpl = function () {
 	return [ 
 		"li", {class:'ui-widget ui-corner-all', id:'task_'+this.id}, [
 			"div", {class:'task-header ui-state-default'}, [
-			    "img", {class:'avatar', src:avatarUrl+'/'+this.assignee}, ,
+			    "img", {class:'ui-icon ui-icon-person avatar', src:avatarUrl+'/'+this.assignee}, ,
 				"div", {class:'head_color', style:'background-color:'+this.color}, ,
 				"div", {id:'color_helper', style:'display:none'}, this.color,
 				"div", {class:'head_name'}, '#' + this.id + ' - ' +this.name,
-				"div", {class:'block-box ' + blockedClass}, ,
-				"span", {class:'ui-icon ui-icon ui-icon-carat-1-n'}, 
+				"span", {class:'block-box ui-icon ui-icon-unlocked not-blocked' + blockedClass}, ,
+				"span", {class:'expander ui-icon ui-icon ui-icon-carat-1-n'}, 
 			],
 			"div", {class:'task-content ui-widget-content', style:'display:block;'}, [
 				"table", {}, [