16a7986b4c9c4c0e4e533f07ca8e0264f3fbabe5
Alexei Bratuhin <alexei.bratuhin@googlemail.com>
2011-09-02 13:12:21 +0200

    * Fix #12, per default auto-archiving task from last workflow column after 30 days

---
+++ test/unit/org/cotabo/AutoArchiveDoneJobTests.groovy
@@ -0,0 +1,43 @@
+package org.cotabo
+
+import grails.test.*
+
+
+class AutoArchiveDoneJobTests extends TaskBoardUnitTest {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testArchive() {
+		//Preparation
+		def col = Column.findByName('done')
+		def user = User.findByUsername('testuser')
+		assertNotNull col
+		assertNotNull user
+		
+		def task = new Task(
+			name: 'mytask',
+			durationHours: 0.5,
+			column: col,
+			creator: user,
+			sortorder: 1,
+			color: '#faf77a',
+			priority: 'Critical',
+			lastUpdated: new Date().minus(31)
+		)
+		assertTrue task.validate()
+		assertNotNull task.save()
+		
+		task = Task.findByName('mytask')
+		assertNotNull task
+		task.lastUpdated = new Date().minus(31)
+		
+		def job = new AutoArchiveDoneJob()
+		job.execute()
+		assertNotNull task.archived
+    }
+}
