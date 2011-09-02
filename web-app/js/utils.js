79212d30c73495069334f5561bebdf3b5a8c6cea
Robert Krombholz <robertkrombholz@gmx.de>
2011-09-02 21:05:21 +0200

    * check if the details are null before replacing their line-endings

--- web-app/js/utils.js
+++ web-app/js/utils.js
@@ -82,7 +82,7 @@ var taskTpl = function () {
 							"td", {}, [
 								"b", {}, 'Details:'									
 							],
-							"td", {}, this.details.replace(/\n/g, '<br/>')							
+							"td", {}, this.details != null ? this.details.replace(/\n/g, '<br/>') : ''							
 						],
 						"tr", {}, [
 							"td", {}, [
