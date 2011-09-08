5b621a66d36c5029e0d254489419612d0ab50849
Robert Krombholz <robertkrombholz@gmx.de>
2011-09-08 17:35:23 +0200

    * Removed dates from import test. They suck with different timezones

--- test/integration/org/cotabo/BoardControllerIntegrationTests.groovy
+++ test/integration/org/cotabo/BoardControllerIntegrationTests.groovy
@@ -30,8 +30,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Setup Webserver</name>
             <priority>Major</priority>
             <sortorder>1</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='4'>
             <archived>false</archived>
@@ -43,8 +41,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Setup WebLogic</name>
             <priority>Low</priority>
             <sortorder>2</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='5'>
             <archived>false</archived>
@@ -56,8 +52,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Install application</name>
             <priority>Critical</priority>
             <sortorder>3</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='6'>
             <archived>false</archived>
@@ -69,8 +63,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Configure Monitoring</name>
             <priority>Normal</priority>
             <sortorder>4</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='7'>
             <archived>false</archived>
@@ -82,8 +74,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Apply Configuration Management</name>
             <priority>Normal</priority>
             <sortorder>5</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
         </tasks>
         <workflowEndColumn>false</workflowEndColumn>
@@ -104,8 +94,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Bootstrap Webserver</name>
             <priority>Critical</priority>
             <sortorder>0</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 03:13:13.013</workflowStartDate>
           </task>
           <task id='2'>
             <archived>false</archived>
@@ -117,8 +105,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Bootstrap App server</name>
             <priority>Normal</priority>
             <sortorder>1</sortorder>
-            <workflowEndDate />
-            <workflowStartDate>2011-04-02 04:13:13.013</workflowStartDate>
           </task>
         </tasks>
         <workflowEndColumn>false</workflowEndColumn>
@@ -139,8 +125,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Request machines</name>
             <priority>Normal</priority>
             <sortorder>3</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 03:13:13.013</workflowStartDate>
           </task>
           <task id='9'>
             <archived>false</archived>
@@ -152,8 +136,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 1</name>
             <priority>Normal</priority>
             <sortorder>12</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='10'>
             <archived>false</archived>
@@ -165,8 +147,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 2</name>
             <priority>Normal</priority>
             <sortorder>13</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='11'>
             <archived>false</archived>
@@ -178,8 +158,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 3</name>
             <priority>Normal</priority>
             <sortorder>14</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='12'>
             <archived>false</archived>
@@ -191,8 +169,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 4</name>
             <priority>Normal</priority>
             <sortorder>15</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='13'>
             <archived>false</archived>
@@ -204,8 +180,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 5</name>
             <priority>Normal</priority>
             <sortorder>16</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='14'>
             <archived>false</archived>
@@ -217,8 +191,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 6</name>
             <priority>Normal</priority>
             <sortorder>17</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='15'>
             <archived>false</archived>
@@ -230,8 +202,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 7</name>
             <priority>Normal</priority>
             <sortorder>18</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='16'>
             <archived>false</archived>
@@ -243,8 +213,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 8</name>
             <priority>Normal</priority>
             <sortorder>19</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='17'>
             <archived>false</archived>
@@ -256,8 +224,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 9</name>
             <priority>Normal</priority>
             <sortorder>20</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
           <task id='18'>
             <archived>false</archived>
@@ -269,8 +235,6 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
             <name>Task 10</name>
             <priority>Normal</priority>
             <sortorder>21</sortorder>
-            <workflowEndDate>2011-04-02 17:13:13.013</workflowEndDate>
-            <workflowStartDate>2011-04-02 13:13:13.013</workflowStartDate>
           </task>
         </tasks>
         <workflowEndColumn>false</workflowEndColumn>
@@ -283,7 +247,8 @@ class BoardControllerIntegrationTests extends GroovyTestCase {
 </boards>'''
 		
 		def exportablesBackup = Task.exportables
-		Task.exportables = exportablesBackup - ['blocks']
+		//Those elements are removed because Date's suck in testing
+		Task.exportables = exportablesBackup - ['blocks', 'workflowStartDate', 'workflowEndDate']
 		
 		def boardcontroller = new BoardController();
 		def result = boardcontroller.export();
