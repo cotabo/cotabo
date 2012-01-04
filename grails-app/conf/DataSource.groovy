c72330ca190c540b3db31514ca13d8d628beec47
Robert Krombholz <robertkrombholz@gmx.de>
2012-01-04 19:55:24 +0100

    * using h2 in test env

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -21,7 +21,7 @@ environments {
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
+            url = "jdbc:h2:mem:testDb"
         }
     }
     production {
