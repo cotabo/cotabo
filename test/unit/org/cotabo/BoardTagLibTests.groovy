e912e30ff511b1f7c03af81b59553a12c9eb655b
Robert Krombholz <robertkrombholz@gmx.de>
2012-01-24 18:21:31 +0100

    * Fixing TagLib tests (... again)

--- test/unit/org/cotabo/BoardTagLibTests.groovy
+++ test/unit/org/cotabo/BoardTagLibTests.groovy
@@ -18,27 +18,27 @@ class BoardTagLibTests extends TagLibUnitTestCase {
 		def expected = """
 		<div id="delete_dialog" class="modal hide">
 			<div class="modal-header">
-				<a href="#" class="close">&times;</a>
-				<h3>Delete Board</h3>
+			    <a href="#" class="close">&times;</a>
+		    	<h3>Delete Board</h3>
 			</div>
-		<div class="modal-body">
+	    <div class="modal-body">
 		<p>Do you really want to delete this board?</p>
 		</div>
 			<div class="modal-footer">
-			<a href="#" class="btn primary">Delete</a>
-			  <a href="#" class="btn secondary">Cancel</a>
-			</div>
+	    	<a href="#" class="btn primary">Delete</a>
+	  		<a href="#" class="btn secondary">Cancel</a>
+		    </div>
 		</div>
 		<script type="text/javascript">
 		jQuery(function(){
-			\$('[data-controls-modal]').click(function() {
-			document.modalHelperHref = \$(this).attr('href');
-		});
-			\$('#delete_dialog > .modal-footer > a.secondary').click(function() {
-				\$('#delete_dialog').modal('hide');
-			});
+            \$('[data-controls-modal]').click(function() {
+            document.modalHelperHref = \$(this).attr('href');
+	    });
+            \$('#delete_dialog > .modal-footer > a.secondary').click(function() {
+		        \$('#delete_dialog').modal('hide');
+	        });
 		\$('#delete_dialog').bind('show', function(e) {
-			\$('#delete_dialog > .modal-footer > a.primary').attr('href', document.modalHelperHref)
+			\$('#delete_dialog > .modal-footer > a.primary').attr('href', document.modalHelperHref)		
 		});
 			\$('#delete_dialog').bind('hide', function(e) {
 				\$('#delete_dialog > .modal-footer > a:first').attr('href', '#');
